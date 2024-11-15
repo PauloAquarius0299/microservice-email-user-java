09111273dd41ec5bed99d02358981271ffaeaaa5
Paulo Cesar <paulocgsantos99@gmail.com>
2024-11-15 10:45:23 -0300

    feat: sendemail message

--- email_service/src/main/java/com/paulotech/email_service/consumers/EmailConsumer.java
+++ email_service/src/main/java/com/paulotech/email_service/consumers/EmailConsumer.java
@@ -2,6 +2,7 @@ package com.paulotech.email_service.consumers;
 
 import com.paulotech.email_service.dtos.EmailRecordDTO;
 import com.paulotech.email_service.models.EmailModel;
+import com.paulotech.email_service.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,9 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
 
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService){
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}" )
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO){
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDTO , emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
